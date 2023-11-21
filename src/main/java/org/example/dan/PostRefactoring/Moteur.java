e8910eb212909a5c36925a27805a02ef620cd609
tom <tom.simul@gmail.com>
2023-11-21 11:15:58 +0100

    Refractor Extract Class

---
+++ src/main/java/org/example/dan/PostRefactoring/Moteur.java
@@ -0,0 +1,56 @@
+package org.example.dan.PostRefactoring;
+
+public class Moteur {
+    private String moteurType;
+    private String moteurCylindree;
+    private String moteurPuissance;
+    private String moteurCarburant;
+
+    public Moteur() {
+    }
+
+    public String getMoteurType() {
+        return moteurType;
+    }
+
+    public void setMoteurType(String moteurType) {
+        this.moteurType = moteurType;
+    }
+
+    public String getMoteurCylindree() {
+        return moteurCylindree;
+    }
+
+    public void setMoteurCylindree(String moteurCylindree) {
+        this.moteurCylindree = moteurCylindree;
+    }
+
+    public String getMoteurPuissance() {
+        return moteurPuissance;
+    }
+
+    public void setMoteurPuissance(String moteurPuissance) {
+        this.moteurPuissance = moteurPuissance;
+    }
+
+    public String getMoteurCarburant() {
+        return moteurCarburant;
+    }
+
+    public void setMoteurCarburant(String moteurCarburant) {
+        this.moteurCarburant = moteurCarburant;
+    }
+
+    public String getMoteurInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Moteur: ");
+        sb.append(moteurType);
+        sb.append(" ");
+        sb.append(moteurCylindree);
+        sb.append(" ");
+        sb.append(moteurPuissance);
+        sb.append(" ");
+        sb.append(moteurCarburant);
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/example/dan/PostRefactoring/Voiture.java
@@ -0,0 +1,69 @@
+package org.example.dan.PostRefactoring;
+
+public class Voiture {
+    private final Moteur moteur = new Moteur();
+    private String marque;
+    private String modele;
+
+    public Voiture(String marque, String modele, String moteur_type, String moteur_cylindree, String moteur_puissance, String moteur_carburant) {
+        this.marque = marque;
+        this.modele = modele;
+        this.moteur.setMoteurType(moteur_type);
+        this.moteur.setMoteurCylindree(moteur_cylindree);
+        this.moteur.setMoteurPuissance(moteur_puissance);
+        this.moteur.setMoteurCarburant(moteur_carburant);
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getMoteurType() {
+        return moteur.getMoteurType();
+    }
+
+    public void setMoteurType(String moteurType) {
+        moteur.setMoteurType(moteurType);
+    }
+
+    public String getMoteurCylindree() {
+        return moteur.getMoteurCylindree();
+    }
+
+    public void setMoteurCylindree(String moteurCylindree) {
+        moteur.setMoteurCylindree(moteurCylindree);
+    }
+
+    public String getMoteurPuissance() {
+        return moteur.getMoteurPuissance();
+    }
+
+    public void setMoteurPuissance(String moteurPuissance) {
+        moteur.setMoteurPuissance(moteurPuissance);
+    }
+
+    public String getMoteurCarburant() {
+        return moteur.getMoteurCarburant();
+    }
+
+    public void setMoteurCarburant(String moteurCarburant) {
+        moteur.setMoteurCarburant(moteurCarburant);
+    }
+
+    public String getMoteurInfo() {
+        return moteur.getMoteurInfo();
+    }
+
+}
