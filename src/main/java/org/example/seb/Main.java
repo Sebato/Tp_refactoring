df2a880eb6e2ffcffb2c162ae518e53605f2b467
Prud'Homme Gateau Sebastien <sebastien.prudhomme-gateau@etu.umontpellier.fr>
2023-11-21 09:57:27 +0100

    structuration

--- src/main/java/org/example/seb/Main.java
+++ src/main/java/org/example/seb/Main.java
@@ -1,7 +1,14 @@
 package org.example.seb;
 
+//changer imports pour tester refactotrings
+
+//Pre Refactoring
 import org.example.seb.preRefactoring.Animal;
 import org.example.seb.preRefactoring.Nourriture;
+
+//Post Refactoring
+//import org.example.seb.postRefactoring.Animal;
+//import org.example.seb.postRefactoring.Nourriture;
 
 public class Main {
     public static void main(String[] args) {
